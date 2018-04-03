16eededdb892ca8806c5c3a8998f371ca0eb99f8
Martgalvan <37112515+Martgalvan@users.noreply.github.com>
2018-04-03 16:36:16 +0200

    Add files via upload

---
+++ openfda2/show_repos3.py
@@ -0,0 +1,19 @@
+import http.client
+import json
+
+headers = {'User-Agent': 'http-client'}
+
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "/drug/label.json?search=active_ingredient:acetylsalicylic&limit=5", None, headers)
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+repos_raw = r1.read().decode("utf-8")
+conn.close()
+
+print(repos_raw)
+
+repos = json.loads(repos_raw)
+
+
+print("The manufacter is", repos['results'][0]['openfda']['manufacturer_name'])
+print("The manufacter is", repos['results'][2]['openfda']['manufacturer_name'])
