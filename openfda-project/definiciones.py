ce68f04aee33aae5e34da1b7ff6688a7c12d930e
marta <marta.galvan.prieto@hotmail.com>
2018-04-24 16:54:14 +0200

    Innovations

--- openfda-project/definiciones.py
+++ openfda-project/definiciones.py
@@ -11,3 +11,11 @@ def active_ingredient(drug,limit):
     repos = json.loads(repos_raw)
     with open('send_info','w'):
         self.wfile.write(bytes('<html><head><h1>OpenFDA Application</h1><h2>you have searched for %.YOu have %' % (drug, limit)), "utf8")
+
+        for i in range(len(repos['results'])):
+            try:
+                for n in range(len(repos['results'][i]["openfda"]["brand_name"])):
+                    try:
+                        self.wfile.write(bytes('<li>'+ 'the active ingredient is'+repos['results'][i]["openfda"]["brand_name"][0] + "</li>", "utf8"))
+                    except KeyError:
+                        break
