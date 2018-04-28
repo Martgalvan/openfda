111432c0ffc78be217f0cab7549852389f70e454
marta <marta.galvan.prieto@hotmail.com>
2018-04-28 14:37:50 +0200

    Innovations

--- openfda-project/server.py
+++ openfda-project/server.py
@@ -21,7 +21,7 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         self.end_headers()
 
 
-        def active_ingredient():
+        def active_ingredient():  #For searching the active ingredient
             headers = {'User-Agent': 'http-client'}
             conn = http.client.HTTPSConnection("api.fda.gov")
             data = self.path.strip('/search?').split('&')
@@ -59,6 +59,43 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
                     el_1 = "<t>" + "<li>" + el
                     f.write(el_1)
 
+        def manufacturer_name():
+            headers = {'User-Agent': 'http-client'}
+            conn = http.client.HTTPSConnection("api.fda.gov")
+            data = self.path.strip('/search?').split('&')
+            manufacturer_name = data[0].split('=')[1]
+            limit = data[1].split('=')[1]
+            print("client has succesfully made a request")
+
+            url = "/drug/label.json?search=openfda.manufacturer_name:" + manufacturer_name + '&' + 'limit=' + limit
+            print(url)
+            conn.request("GET", url, None, headers)
+            r1 = conn.getresponse()
+            repos_raw = r1.read().decode("utf-8")
+            conn.close()
+            repos = json.loads(repos_raw)
+
+            drug = []
+            a = 0
+            nlimit = int(limit)
+            intro = "<head>" + '<h1>' + "Here is your drug list from the manufacturer you searched." + '<body style="background-color:snow;">' + '</h1>' + '</head>'
+            sd = "<ol>"
+
+            while a < nlimit:
+                try:
+                    drug.append(repos['results'][a]['openfda']['brand_name'][0])
+                    a += 1
+
+                except:
+                    a += 1
+                    print("There is no drug with this active ingredient")
+
+            with open("trial4.html", "w") as f:
+                f.write(intro)
+                f.write(sd)
+                for el in drug:
+                    el_1 = "<t>" + "<li>" + el
+                    f.write(el_1)
 
 
         if self.path == "/":
@@ -72,6 +109,13 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             with open("trial4.html", "r") as f:
                 ingr = f.read()
                 self.wfile.write(bytes(ingr, "utf8"))
+
+        elif 'manufacturer' in self.path:
+            manufacturer_name()
+            with open("trial4.html", "r") as f:
+                ingr = f.read()
+                self.wfile.write(bytes(ingr, "utf8"))
+
 
         return
 
